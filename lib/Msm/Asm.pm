afe498741e86bc0a929bbcd4be878fa5bc92bba2
John Berthels <john@localhost>
2012-04-12 20:04:20 +0100

    use printed output for result from asm, to get working -ve numbers

--- lib/Msm/Asm.pm
+++ lib/Msm/Asm.pm
@@ -2,8 +2,9 @@ package Msm::Asm;
 use Modern::Perl;
 use Moose;
 
-has '_data_section', is => 'rw';
+has '_bss_section', is => 'rw';
 has '_text_section', is => 'rw';
+has '_data_section', is => 'rw';
 
 use File::Temp qw(tempfile);
 
@@ -11,24 +12,34 @@ sub run_ast {
     my ($self, $ast) = @_;
 
     $self->_data_section('');
+    $self->_bss_section('');
     $self->_text_section('');
 
     $self->_emit_text($ast->to_asm);
     $self->_emit_text($self->_lib_text);
+    $self->_emit_bss($self->_lib_bss);
     $self->_emit_data($self->_lib_data);
 
     my ($asm_fh, $asm_file) = tempfile('/tmp/asmXXXX', SUFFIX => '.asm', UNLINK => 0);
-    print $asm_fh $self->_data_section;
-    print $asm_fh $self->_text_section;
+    print $asm_fh "section .data\n" . $self->_data_section;
+    print $asm_fh "section .bss\n" . $self->_bss_section;
+    print $asm_fh "section .text\n" . $self->_text_section;
     close $asm_fh or die "can't close asm fh $asm_file : $!";
 
     my ($exe_fh, $exe_file) = tempfile('/tmp/asmXXXX', UNLINK => 1);
     close $exe_fh;
     $self->_compile_asm($asm_file, $exe_file);
 
-    my $result = system($exe_file);
-    $result >>= 8; # get exit code
+#    my $result = system($exe_file);
+#    $result >>= 8; # get exit code
+    my ($result) = `$exe_file`;
+    chomp $result;
     return $result;
+}
+
+sub _emit_data {
+    my ($self, $data) = @_;
+    $self->_data_section($self->_data_section . $data);
 }
 
 sub _emit_text {
@@ -36,9 +47,9 @@ sub _emit_text {
     $self->_text_section($self->_text_section . $text);
 }
 
-sub _emit_data {
+sub _emit_bss {
     my ($self, $text) = @_;
-    $self->_data_section($self->_data_section . $text);
+    $self->_bss_section($self->_bss_section . $text);
 }
 
 sub _compile_asm {
@@ -118,7 +129,6 @@ EOASM
         my ($self) = @_;
 
         my $result = <<"EOPREAMBLE";
-section .text
     global  _start
 
 _start:
@@ -126,6 +136,7 @@ EOPREAMBLE
         my @code = map { $_->to_asm } @{$self->exps};
         $result .= join("\n", @code);
         $result .= <<"EOPOSTAMBLE";
+    call    print_decimal
     pop     rbx
     mov     rax, 1
     int     80h
@@ -139,12 +150,86 @@ sub _lib_text {
 ;
 ; Msm library code
 ;
+print_decimal:
+    mov     rbx, print_decimal_buf
+
+    mov     r8, 0
+
+    cmp     eax, 0x80000000
+    jc      .not_neg
+    mov     ecx, eax
+    neg     eax
+    mov     r8, 1
+
+.not_neg:
+
+.next_digit:
+    mov     rdx, 0
+    mov     ecx, 10
+    idiv    ecx
+
+    add     dl, '0'
+    mov     [rbx], dl
+    inc     rbx
+
+    cmp     rax, 0
+    jnz      .next_digit
+
+    cmp     r8, 1
+    jnz     .dont_print_sign
+    mov     byte [rbx], '-'
+    inc     rbx
+.dont_print_sign:
+
+
+    dec     rbx
+    mov     rcx, rbx
+.print_digit:
+
+    mov     rax, 4
+    mov     rbx, 1
+    mov     rdx, 1
+    int     80h
+    dec     rcx
+    cmp     rcx, print_decimal_buf
+    jnc     .print_digit
+
+    ret
+
+print_newline:
+    push    rax
+    push    rbx
+    push    rcx
+    push    rdx
+
+    mov     rax, 4
+    mov     rbx, 1
+    mov     rcx, newline
+    mov     rdx, 1
+    int     80h
+
+
+    pop     rdx
+    pop     rcx
+    pop     rbx
+    pop     rax
+    ret
+    
 EOLIB
+}
+
+sub _lib_bss {
+    my ($self) = @_;
+    return << "EOBSS";
+    print_decimal_buf:      resb 22
+EOBSS
 }
 
 sub _lib_data {
     my ($self) = @_;
-    return '';
+    return << "EODATA";
+    newline         db 10
+EODATA
 }
 
 1;
